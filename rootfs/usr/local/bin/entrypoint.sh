5759697d9bcb42fb9b04289141be134e7d58e002
Jee <jee@jeer.fr>
2022-01-06 17:24:19 +0100

    absolute path for betanin

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -18,7 +18,7 @@ beets_dir="/config/.config/beets"
 beets_config="/config/.config/beets/config.yaml"
 
 if [ "$MODE" == betanin ]; then
-    betanin
+    /usr/bin/betanin
 elif [ "$MODE" == 'inotifywait' ]; then
     inotifywait -m -e create,moved_to --format '%w%f' "$WATCH_DIR" | while IFS= read -r dir_path; do
         /usr/local/bin/beet -c "$beets_config" import -q -i --flat "$dir_path"
